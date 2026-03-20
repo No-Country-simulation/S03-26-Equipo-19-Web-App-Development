a112c2899077be86c99d90cf3c2806ee09bca092
Marina Victoria Blanco <marivic_05@hotmail.com>
2026-03-20 18:19:49 -0300

    Agrego endpoints y variables de entorno

--- frontend/src/services/general_api.tsx
+++ frontend/src/services/general_api.tsx
@@ -4,8 +4,8 @@ export const apiAuthService = axios.create({
     baseURL: `${import.meta.env.VITE_URL_BASE}/auth`
 })
 
-export const apiUsersService = axios.create({
-    baseURL: `${import.meta.env.VITE_URL_BASE}/users`
+export const apiContactsService = axios.create({
+    baseURL: `${import.meta.env.VITE_URL_BASE}/contacts`
 })
 
 export const apiMessagesService = axios.create({
