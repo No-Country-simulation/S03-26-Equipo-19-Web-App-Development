75ac2588a925b514949349cd4c76c42d5f63fb71
anthonybanion <abanon@fi.uba.ar>
2026-04-01 22:21:52 -0300

    sacando frontend de backend Revert "Merge pull request #33 from No-Country-simulation/frontend"
    This reverts commit 762a467e5b24bf480eb727a4c8cab4e1feae699e, reversing
    changes made to 6f72c3ec9523d0ba097945095f72cd375e349e6e.

--- frontend/src/pages/DashboardPage.tsx
+++
@@ -1,8 +0,0 @@
-// src/pages/DashboardPage.tsx
-import { DashboardLayout } from '../components/layout/DashboardLayout';
-
-export const DashboardPage = () => {
-  return (
-    <DashboardLayout />    
-  );
-}
