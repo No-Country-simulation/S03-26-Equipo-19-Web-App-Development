fcb0788a00660b59d80ebeb3919af1349335247e
anthonybanion <abanon@fi.uba.ar>
2026-04-10 18:02:41 -0300

    [Backend]=> Borra dependencias sin uso

--- backend/src/main/java/com/crm/app/model/Tag.java
+++ backend/src/main/java/com/crm/app/model/Tag.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Etiqueta de clasificación libre definida por el Admin.
