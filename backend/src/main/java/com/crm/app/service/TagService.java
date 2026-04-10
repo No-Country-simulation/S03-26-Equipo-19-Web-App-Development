35a304bc63b4c3b1b15926b05b00b6cc2cfee74e
anthonybanion <abanon@fi.uba.ar>
2026-04-10 20:27:35 -0300

    [Backend]=> Add transacciones

---
+++ backend/src/main/java/com/crm/app/service/TagService.java
@@ -0,0 +1,10 @@
+package com.crm.app.service;
+
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TagService {
+    
+}
